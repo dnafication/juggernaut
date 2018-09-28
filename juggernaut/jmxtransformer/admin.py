bfb5e871aa58b37b8a72c52cccdb5e9133d45040
dnafication <>
2018-09-28 17:08:44 +1000

    deleted old django app

--- juggernaut/jmxtransformer/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Jmx
-
-admin.site.register(Jmx)
--- juggernaut/jmxtransformer/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JmxtransformerConfig(AppConfig):
-    name = 'jmxtransformer'
